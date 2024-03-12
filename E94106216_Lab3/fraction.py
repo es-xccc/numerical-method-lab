0e1ac292d083a6f8775537cdac35123b2663d40a
es-xccc <r2605572@gmail.com>
2024-03-12 16:31:48 +0800

    lab3

--- E94106216_Lab3/fraction.py
+++ E94106216_Lab3/fraction.py
@@ -8,26 +8,22 @@ class Fraction(object):
     def __add__(self, other): # +
         new_num = self.num * other.den + self.den * other.num
         new_den = self.den * other.den
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __sub__(self,other): # -
         new_num = self.num * other.den - self.den * other.num
         new_den = self.den * other.den
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __mul__(self,other): # *
         new_num = self.num * other.num
         new_den = self.den * other.den
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __truediv__(self,other): # /
         new_num = self.num * other.den
         new_den = self.den * other.num
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __eq__(self,other): # ==
         return self.num * other.den == self.den * other.num
@@ -36,7 +32,13 @@ class Fraction(object):
         return self.num * other.den != self.den * other.num
 
     def __repr__(self):
+        common = gcd(self.num, self.den)
+        self.num //= common
+        self.den //= common
         return "Numerator:{0}\nDenominator:{1}".format(self.num, self.den)
 
     def __str__(self):
+        common = gcd(self.num, self.den)
+        self.num //= common
+        self.den //= common
         return "{0}/{1}".format(self.num, self.den)
